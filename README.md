9f36b3aa68c93abe8da3f9a2c4f17b38be78ce8e
Marco Kellershoff <marco@kellershoff.net>
2025-02-15 19:19:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+<div align="center">
+
+![Zana Logo](assets/logo.svg)
+
+# zana.nvim
+
+[![Made with love](assets/badge-made-with-love.svg)](https://github.com/mistweaverco/zana.nvim/graphs/contributors)
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/mistweaverco/zana.nvim?style=for-the-badge)](https://github.com/mistweaverco/zana.nvim/releases/latest)
+[![Discord](assets/badge-discord.svg)](https://getzana.net/discord)
+
+[Requirements](#requirements) • [Install](#install) • [Documentation](https://neovim.getzana.net/)
+
+<p></p>
+
+Zana 📦 is Mason.nvim 🧱, but maintained by the community 🌈.
+
+Zana is swahili for "tools" or "tooling".
+
+A thin layer for [Zana](https://github.com/mistweaverco/zana-gui),
+the package manager for Neovim.
+
+<p></p>
+
+</div>
+
+## Requirements
+
+- Neovim 0.10.0+
+
+## Install
+
+Via [lazy.nvim](https://github.com/folke/lazy.nvim):
+
+### Configuration
+
+```lua
+{ 'mistweaverco/zana.nvim', opts = {} },
+```
+
+> `opts` needs to be an empty table.
+
---
+++ lua/zana/init.lua
@@ -0,0 +1,54 @@
+local function normalize_path(path)
+  local parts = {}
+  local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
+
+  -- Convert Windows backslashes to forward slashes for consistency
+  if is_windows then
+    path = path:gsub("\\", "/")
+  end
+
+  for part in string.gmatch(path, "[^/]+") do
+    if part == ".." then
+      if #parts > 0 then
+        table.remove(parts) -- Go up one directory
+      end
+    elseif part ~= "." and part ~= "" then
+      table.insert(parts, part)
+    end
+  end
+
+  local normalized = (is_windows and "" or "/") .. table.concat(parts, "/")
+
+  -- Convert backslashes for Windows paths
+  if is_windows then
+    normalized = normalized:gsub("/", "\\")
+  end
+
+  return normalized
+end
+
+local IS_WINDOWS = vim.loop.os_uname().sysname == "Windows_NT"
+local PS = IS_WINDOWS and "\\" or "/"
+local PATH_SEPARATOR = IS_WINDOWS and ";" or ":"
+
+local getHomePath = function()
+  local path = vim.fn.stdpath("config") .. PS .. ".." .. PS .. "zana"
+  return normalize_path(path)
+end
+
+local getAppPackagesPath = function()
+  return getHomePath() .. PS .. "packages"
+end
+
+local getNPMPath = function()
+  return getAppPackagesPath() .. PS .. "npm" .. PS .. "node_modules" .. PS .. ".bin"
+end
+
+vim.notify("Setting PATH to " .. getNPMPath(), "info")
+
+vim.env.PATH = getNPMPath() .. PATH_SEPARATOR .. vim.env.PATH
+vim.fn.setenv("PATH", vim.env.PATH)
+
+return {
+  setup = function() end,
+}
